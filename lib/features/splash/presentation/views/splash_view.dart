b2f1111cb713a55ce9fc7dfc9fdd86691448fccf
OmarTalaat <omartalaaat53@gmail.com>
2025-04-26 20:42:15 +0300

    splash view ui done

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,4 +1,3 @@
-import 'package:bookly_app_student/features/splash/presentation/views/widgets/splash_view_body.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
@@ -6,6 +5,6 @@ class SplashView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SplashViewBody();
+    return const Scaffold(backgroundColor: Color(0xff100B20));
   }
 }
